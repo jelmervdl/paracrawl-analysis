f842c49afba55a2cbda0f9979c7a59bfadc4854b
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-04-16 15:41:00 +0100

    Auxiliary functions
    For displaying & sampling data

---
+++ notebook.py
@@ -0,0 +1,67 @@
+import html
+from typing import Generator
+from itertools import chain
+
+
+def first(it, default=None):
+    return next(iter(it), default)
+
+
+def peek(iterable):
+    try:
+        first = next(iterable)
+        return first, chain([first], iterable)
+    except StopIteration:
+        return None, iter([])
+
+def isgenerator(iterable):
+    return hasattr(iterable, '__iter__') and not hasattr(iterable, '__len__')
+
+def isiterable(iterable):
+    return hasattr(iterable, '__iter__')
+
+class table:
+    def __init__(self, data, *, headers=None):
+        self.data = data
+        self.headers = headers
+    
+    def _repr_html_val(self, val, headers=None):
+        if val is None:
+            return '<em>None</em>'
+        elif hasattr(val, '_repr_html_'):
+            return val._repr_html_()
+        elif isinstance(val, str):
+            return html.escape(val)
+        elif isinstance(val, (int, float)):
+            return str(val)
+        elif isiterable(val):
+            first_row, rows = peek(iter(val))
+            
+            if isinstance(first_row, (str, float)):
+                rows = map(lambda row: [row], rows)
+            
+            if headers is None and isinstance(first_row, dict):
+                headers = list(first_row.keys())
+            
+            if headers:
+                html_head = ('<th>{}</th>'.format(self._repr_html_val(header)) for header in headers)
+            else:
+                html_head = ''
+
+            html_rows = (
+                '<tr>{}</tr>'.format(''.join(
+                    '<td>{}</td>'.format(self._repr_html_val(val))
+                    for val in (row.values() if isinstance(row, dict) else row)
+                )) for row in rows
+            )
+
+            return '<table><thead>{head}</thead><tbody>{rows}</tbody></table>'.format(
+                head='<tr>{}</tr>'.format(''.join(html_head)) if html_head else '',
+                rows='\n'.join(html_rows))
+        else:
+            return html.escape(repr(val))
+    
+    def _repr_html_(self):
+        return self._repr_html_val(self.data, headers=self.headers)
+            
+        
---
+++ stats.py
@@ -0,0 +1,37 @@
+import random
+import warnings
+
+class PopulationSizeWarning(Warning):
+    pass
+
+class RandomSample:
+    def __init__(self, size):
+        self.size = size
+        self.n = 0
+        self._sample = []
+    
+    def __iter__(self):
+        if self.n < self.size:
+            warnings.warn('Population (size {}) smaller than sample (size {})'.format(self.n + 1, self.size), PopulationSizeWarning)
+        return iter(self._sample)
+    
+    def __len__(self):
+        return len(self._sample)
+        
+    def add(self, item):
+        if self.n < self.size:
+            self._sample.append(item)
+            self.n += 1
+            if self.n == self.size:
+                random.shuffle(self._sample)
+        else:
+            pos = random.randint(0, self.n)
+            if pos < self.size:
+                self._sample[pos] = item
+            self.n += 1
+
+    def update(self, items):
+        for item in items:
+            self.add(item)
+
+
